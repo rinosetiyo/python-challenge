d83c58d0a5b3546640385902d8e9c0cf38310403
rinosetiyo <42023075+rinosetiyo@users.noreply.github.com>
2022-11-02 04:20:12 +0700

    Update blackjack.py

--- blackjack.py
+++ blackjack.py
@@ -1,4 +1,17 @@
 import random
+
+logo = """
+
+__________.__                 __          ____.              __       ________                              
+\______   \  | _____    ____ |  | __     |    |____    ____ |  | __  /  _____/_____    _____   ____   ______
+ |    |  _/  | \__  \ _/ ___\|  |/ /     |    \__  \ _/ ___\|  |/ / /   \  ___\__  \  /     \_/ __ \ /  ___/
+ |    |   \  |__/ __ \\  \___|    <  /\__|    |/ __ \\  \___|    <  \    \_\  \/ __ \|  Y Y  \  ___/ \___ \ 
+ |______  /____(____  /\___  >__|_ \ \________(____  /\___  >__|_ \  \______  (____  /__|_|  /\___  >____  >
+        \/          \/     \/     \/               \/     \/     \/         \/     \/      \/     \/     \/ 
+"""
+
+print(logo)
+print("-----------------------------------------------------")
 card_list = [2,3,4,5,6,7,8,9,10,11]
 
 first_card = random.choice(card_list)
@@ -12,16 +25,24 @@ print(f"Your first card {first_card} and your second card {second_card} and tota
 print(f"Computer card is {computer_card}")
 
 while my_total <= 16:
-  print("You have to add more card !")
-  more_card = input("Click 'Y' to add more card : ").lower()
-  if more_card == "y":
-    add_card = random.choice(card_list)
-    my_total += add_card
-    print(f"Your more card {add_card} and total {my_total} ")
+    print("You have to add more card !")
+    more_card = input("Click 'Y' to add more card : ").lower()
+    if more_card == "y":
+      add_card = random.choice(card_list)
+      my_total += add_card
+      print(f"Your more card {add_card} and total {my_total} ")
+while computer_card <=16:
+    com_more_card = random.choice(card_list)
+    computer_card += com_more_card
+    print(f"computer add {com_more_card} and total {computer_card}")
   
-if my_total > computer_card:
+if my_total > 21:
+  print("You lose")
+elif computer_card > 21:
+  print("You win")
+elif my_total > computer_card:
   print("You win")
 elif my_total < computer_card:
   print("You lose")
 elif my_total == computer_card:
-  print("Draw !")
+  print("Draw !")     
